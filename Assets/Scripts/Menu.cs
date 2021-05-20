fb4ea36aa94559352095493091f0feb862048eca
J <t.frachet@etudiant.ecolemelies.fr>
2021-05-20 15:06:33 +0200

    menu fonctionnel except play

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void play()
+    {
+        SceneManager.LoadScene("Platformer");
+    }
+
+    public void help()
+    {
+        SceneManager.LoadScene("Help");
+    }
+
+    public void credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void main()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
